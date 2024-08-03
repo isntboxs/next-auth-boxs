4a6f8b2f529dd104785e7caf569dced40df94fce
mrboxs <rizkywanda200155@gmail.com>
2024-08-03 10:10:27 +0700

    create user & test db

--- actions/register-action.tsx
+++ actions/register-action.tsx
@@ -1,6 +1,10 @@
 "use server"
 
+import { getUserByEmail } from "@/data/user"
 import { RegisterSchema, RegisterSchemaType } from "@/schemas"
+import bcrypt from "bcrypt"
+
+import { db } from "@/lib/db"
 
 export const REGISTER_ACTION = async (value: RegisterSchemaType) => {
   const validatedFields = RegisterSchema.safeParse(value)
@@ -9,5 +13,28 @@ export const REGISTER_ACTION = async (value: RegisterSchemaType) => {
     return { error: "invalid email or password" }
   }
 
-  return { success: "email sent!" }
+  const { name, email, password } = validatedFields.data
+  // hash password
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  // check if user already exists
+  const existingUser = await getUserByEmail(email)
+
+  // if user already exists by email return error
+  if (existingUser) {
+    return { error: "email already registered" }
+  }
+
+  // create user in db
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  })
+
+  // todo: send verification token to email
+
+  return { success: "user created!" }
 }
